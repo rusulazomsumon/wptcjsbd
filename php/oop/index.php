0dd066abf9e30eee6a79eeaaca49349bbea332f3
RusulAzomSumon <18103020@iubat.edu>
2022-10-17 01:09:15 +0600

    php programing oop

---
+++ php/oop/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>পিএইচপি(PHP) দিয়ে অবজেক্ট ওরিয়েন্টেড প্রোগ্রামিং (OOP) (বাংলায়)</title>
+</head>
+<body>
+    <h1>সূচিপত্র</h1>
+    <ul>
+        <li>বেসিক আলোচনা</li>
+        <li>অবজেক্ট</li>
+        <li>ক্লাস</li>
+        <li>কন্সট্রাক্টর</li>
+        <li>ডেস্ট্রাক্টর</li>
+        <li>ইনহেরিটেন্স</li>
+        <li>এনক্যাপচুলেসন</li>
+        <li>আবসট্রাকসন</li>
+        <li>ইন্টারফেস</li>
+        <li>স্ট্যাটিক মেম্বার</li>
+        <li>ত্রেইট</li>
+        <li>পলিমরফিজম</li>
+        <li>অন্যান্য</li>
+    </ul>
+</body>
+</html>
