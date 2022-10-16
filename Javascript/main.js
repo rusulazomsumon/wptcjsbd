78aa2e10818d451cfe442d6da79db0c0e32ee2b5
RusulAzomSumon <18103020@iubat.edu>
2022-10-16 23:56:25 +0600

    javascript basic revition

---
+++ Javascript/main.js
@@ -0,0 +1,31 @@
+        // 01. introduction 
+        /* 
+        * Where we can tun JS
+        * Using node js
+        * In browser console
+        * In browser snipet
+        */
+
+        // 02. Variable
+        /* 
+        * let = most used.
+        * var
+        * const
+        */ 
+
+        // 03. Data type
+        /* 
+        * String 
+        * Number
+        * Boolean
+        * Array 
+        * function
+        * Object
+        * null
+        * undefined
+        */ 
+        // typeof null is a object but it is a error
+        // let n = null;
+        // console.log(typeof n);
+        // let n = null;
+        // console.log(typeof n);
