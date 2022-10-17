0cf2ae64513867044983bf7f27b8797014e85922
RusulAzomSumon <18103020@iubat.edu>
2022-10-17 21:08:27 +0600

    jQuery Added

---
+++ jQuery/main.js
@@ -0,0 +1,15 @@
+// lets let the whole documents load
+$(document).ready(function(){
+    // jQuery Codes
+    // // #####clic button hide and show image
+    // $("button").click(function(){
+    //     $("img").toggle(2000);
+    // });
+
+    // // ######click button change color of text and restore#######
+    // $("button").click(function(){
+    //     $("h1").css('color','red');
+    // });
+    
+
+});
