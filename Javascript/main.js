9a8d7460fd62f6be12da85a6b91a350f6f5e23b8
Md. Rusul Azom <37295674+rusulazomsumon@users.noreply.github.com>
2022-10-18 17:27:55 +0600

    Update main.js
    object in js

--- Javascript/main.js
+++ Javascript/main.js
@@ -51,3 +51,26 @@
         // let b = 6;
         // let max = a>b ? 3:6;
         // console.log(max);
+
+        // JS BD lecture: objects
+
+        let student = {
+            name: 'babu',
+            age: 25,
+            skill: {
+                language: 'PHP',
+                freamwork: 'laravel',
+                CMS: 'WordPress'
+            },
+            add: function (x, y) {
+                return x+y;
+            },
+            sayHi: function () {
+                console.log("Hi! Im Sumon");
+            },
+        }
+        // console.log(student['age']);
+        // console.log(student.name);
+        // console.log(student.skill.CMS);
+        // console.log(student.add(5,6));
+        console.log(student.sayHi());
