961952a2e2dbd96bb3e131bcf7b78df54b893a47
RusulAzomSumon <18103020@iubat.edu>
2022-10-20 20:53:10 +0600

    js dom

---
+++ Javascript/DOM/main.js
@@ -0,0 +1,6 @@
+// ########### Dom Selectors #############
+// Best Selector is Query Selector
+const title = document.querySelector('h1');
+// Dom manipulation 
+title.style.color = "#5e05e0";
+title.innerHTML = '<h6>H1 to H6</h6>';
