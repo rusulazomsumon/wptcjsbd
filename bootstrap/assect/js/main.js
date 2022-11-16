3c2d64ab8ce8c0713d163aa03e540ad55ad42b45
RusulAzomSumon <18103020@iubat.edu>
2022-11-16 23:55:50 +0600

    footer update

--- bootstrap/assect/js/main.js
+++ bootstrap/assect/js/main.js
@@ -1,3 +1,22 @@
+// *****************Accordiation*********************
+function openProject(evt, projectName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(projectName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+
 //********************* */ Back to Top Button ****************
 // Selection the button by class name
 const backToTop = document.querySelector(".back-to-top");
@@ -15,3 +34,4 @@ window.addEventListener("scroll", () => {
 var currentYear = new Date().getFullYear();
 var copyElmts = document.querySelector(".year");
 copyElmts.innerHTML = currentYear;
+
